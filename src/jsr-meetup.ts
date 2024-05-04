ac6e503b653ffdea99f4b80f79494552932da412
Octo8080 <okutannderuta0529@gmail.com>
2024-05-04 14:55:47 +0900

    update JSDoc

--- src/jsr-meetup.ts
+++ src/jsr-meetup.ts
@@ -1,3 +1,13 @@
+/**
+ * console output JSR Meetup 2024-05-03 in 台東区民会館
+ * @returns ThothClient
+ *
+ * @example
+ * ```ts
+ * import {call} from "jsr:@octo/jsr-meetup";
+ * call()
+ * ```
+ */
 export function call(){
   console.log("JSR Meetup 2024-05-03 in 台東区民会館 ! https://deno-ja.connpass.com/event/316018/")
 }
